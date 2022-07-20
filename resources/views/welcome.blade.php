db45366fe649cf7e06560f03d1101bcc11c27786
abby2727 <abdulfahadgo566@gmail.com>
2022-07-20 19:41:50 +0800

    Update welcome.blade

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,8 +2,7 @@
 
 @section('content')
     <div class="text-center">
-        <p class="text-danger">Hello, world!</p>
-        <h3>Create, Read and Modify Employee</h3>
+        <h3 class="text-danger">Create, Read and Modify Employee</h3>
         <h3>with this Simple Website. Update</h3>
     </div>
 @endsection
